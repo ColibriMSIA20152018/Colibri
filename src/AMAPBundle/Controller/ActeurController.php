a82e5c530d7a6bd1f2e59d459d8a0579c2f59c35
FabienBrG <waofab36@hotmail.fr>
2016-05-03 16:52:20 +0200

    Librairie JS DataTables + Importation Template Solarize
    J'ai trouver une librairie plutot pas mal pour les tableaux j'vous
    laisse découvrir çà.
    
    J'ai juste importer les fichier du template pour le moment je l'ai pas
    appliquer !

--- src/AMAPBundle/Controller/ActeurController.php
+++ src/AMAPBundle/Controller/ActeurController.php
@@ -33,7 +33,7 @@ class ActeurController extends Controller
 		if ($form->handleRequest($request)->isSubmitted()){
 			$data = $form->getData();
 			$session =  $request->getSession();
-                        
+
                         $session->set('amap',$data['amap']->getId());
 			return $this->redirectToRoute('amap_acteur', array('amap' => $data['amap']->getId()));
 		}
@@ -112,11 +112,17 @@ class ActeurController extends Controller
         }
 
         $listacteur = $em->getRepository('AMAPBundle:Acteur')->findBy(array('amap' => $amap));
+		$typeProd = $em->getRepository('AMAPBundle:TypeActeur')->findBy(array('libelle' => "Producteur"));
+		$typeAd =$em->getRepository('AMAPBundle:TypeActeur')->findBy(array('libelle' => "Consommateur"));
+		$listProd = $em->getRepository('AMAPBundle:Acteur')->findBy(array('amap' => $amap, 'typeActeur' => $typeProd));
+		$listAd = $em->getRepository('AMAPBundle:Acteur')->findBy(array('amap' => $amap, 'typeActeur' => $typeAd));
 
         return $this->render('AMAPBundle:Acteur:index.html.twig',array(
             'form' => $form->createView(),
             'form2' => $form2->createView(),
             'page_courante' => 'acteur',
-            'listacteur' => $listacteur));
+            'listacteur' => $listacteur,
+			'listProd' => $listProd,
+			'listAd' => $listAd));
     }
 }
