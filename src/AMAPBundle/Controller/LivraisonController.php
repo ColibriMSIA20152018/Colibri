defa6cecca7882bc6fca9da4d5ed33cf5b349b68
FabienBrG <waofab36@hotmail.fr>
2016-05-05 01:18:23 +0200

    Livraison presque Done :+1:
    Bon il faut que je regarde pour effectuer une livraison sur un panier
    donné et à une date précise et pas que sa fasse la livraison de tous les
    panier peu importe la date.
    
    J'ai fais également la livraison d'un panier tout seul lorsque l'on
    choist le panier et plutard la date via l'icone check à coté de l'état
    de livraison ;)

--- src/AMAPBundle/Controller/LivraisonController.php
+++ src/AMAPBundle/Controller/LivraisonController.php
@@ -5,6 +5,7 @@ namespace AMAPBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use \AMAPBundle\Entity\Produit;
+use \AMAPBundle\Entity\Contrat;
 use \AMAPBundle\Entity\Stock;
 use \AMAPBundle\Entity\Famille;
 use \AMAPBundle\Entity\Saison;
@@ -33,7 +34,7 @@ class LivraisonController extends Controller
 
 		$form2 = $this->get('form.factory')->createNamedBuilder('formulaire_effectuer_livraison')
             ->add('panier', EntityType::class, array('class' => 'AMAPBundle:Panier', 'choice_label' => 'libelle'))
-            ->add('livrer', SubmitType::class, array('label' => 'Effectuer la livraison'))
+            ->add('livrer', SubmitType::class, array('label' => 'Préparer la livraison'))
             ->getForm();
 
 
@@ -60,21 +61,21 @@ class LivraisonController extends Controller
 
                 $em->flush();
             }
-			if ($form2->get('livrer')->isClicked())
-			{
-				$data = $form2->getData();
+            if ($form2->get('livrer')->isClicked())
+            {
+		$data = $form2->getData();
 
-				return $this->redirect($this->generateUrl('amap_effectue_livraison', array('id'=>$data['panier']->getId())));
-			}
+		return $this->redirect($this->generateUrl('amap_effectue_livraison', array('id'=>$data['panier']->getId())));
+            }
         }
 
 		$listContrat = $em->getRepository('AMAPBundle:Contrat')->findAll();
 
 
         return $this->render('AMAPBundle:Livraison:index.html.twig',array('form' => $form->createView(),
-																			'form2' => $form2->createView(),
-																			'page_courante' => 'livraison',
-																			'listContrat' => $listContrat));
+                                                                            'form2' => $form2->createView(),
+                                                                            'page_courante' => 'livraison',
+                                                                            'listContrat' => $listContrat));
     }
 
 	public function effectuerAction(Request $request,$id)
@@ -83,15 +84,57 @@ class LivraisonController extends Controller
 		
 		$listContrat = $em->getRepository('AMAPBundle:Contrat')->findBy(array('panier' => $id));
 
-		$form = $this->get('form.factory')->createNamedBuilder('formulaire_livraison')
-            ->add('dateTime', DateTimeType::class)
-            ->add('contrat',EntityType::class, array('class' => 'AMAPBundle:Contrat', 'choice_label' => 'id'))
-            ->add('ajouter', SubmitType::class, array('label' => 'Ajouter livraison'))
-            ->getForm();
+		$form = $this->get('form.factory')->createNamedBuilder('formulaire_effectuer_livraison')
+                            ->add('livrer', SubmitType::class, array('label' => 'Effectuer la livraison'))
+                            ->getForm();
 
+                if ($form->handleRequest($request)->isSubmitted()){
+                    if ($form->get('livrer')->isClicked()){
+                        foreach ($listContrat as $contrat) {
+                            foreach ($contrat->getLivraisons() as $livraison) {
+                                $livraison->setEstLivree(true);
+                                
+                                $em->persist($livraison);                                
+                            }
+                        }
+                    }
+                    
+                    $em->flush();
+                    return $this->redirect($this->generateUrl('amap_effectue_livraison', array('id'=>$id)));
+                }
+                
+                
 		return $this->render('AMAPBundle:Livraison:effectuer.html.twig',array('form' => $form->createView(),
-																			'page_courante' => 'livraison',
-																			'listContrat' => $listContrat));
+                                                                                    'page_courante' => 'livraison',
+                                                                                    'listContrat' => $listContrat));
 	}
+        
+        public function effectuerOneAction($idpanier,$id,$date){
+            $em = $this->getDoctrine()->getManager();
+          
+            $form = $this->get('form.factory')->createNamedBuilder('formulaire_effectuer_livraison')
+                            ->add('livrer', SubmitType::class, array('label' => 'Effectuer la livraison'))
+                            ->getForm();
+            
+            $contrat = $em->getRepository('AMAPBundle:Contrat')->findBy(array('id' => $id));
+            $listContrat = $em->getRepository('AMAPBundle:Contrat')->findBy(array('panier' => $idpanier));
+            
+            foreach ($contrat[0]->getLivraisons() as $livraison) {
+               
+                if($livraison->getDateLivraison()->format('Y-m-d H:i:s') == $date)
+                {
+                    var_dump('ici');
+                    $livraison->setEstLivree(true);
+                    $em->persist($livraison);
+                }
+            }
+            
+            $em->flush();
+            
+            return $this->render('AMAPBundle:Livraison:effectuer.html.twig', array('form' => $form->createView(),
+                                                                                    'page_courante' => 'livraison',
+                                                                                    'listContrat' => $listContrat));
+            
+        }
 
 }
